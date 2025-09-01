6a156100bcc26f223faca21c1ee16378ba36d496
Negin Ahmadi <negin.ahmadi.tech@gmail.com>
2025-09-01 16:38:36 -0700

    feat: :sparkles: Showing the entered value using useState hook

--- code/08 Refs Portals/01-starting-project/src/components/Player.jsx
+++ code/08 Refs Portals/01-starting-project/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [name, setName] = useState("");
+  const [submit, setSubmit] = useState(false);
+
+  function handleChange(e) {
+    setSubmit(false);
+    setName(e.target.value);
+  }
+
+  function handleClick() {
+    setSubmit(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submit ? name : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={name} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
